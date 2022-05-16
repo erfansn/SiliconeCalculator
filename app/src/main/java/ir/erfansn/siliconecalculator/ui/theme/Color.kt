a31f25d77fe41d3f81de75652fb6a5f02cbea50d
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-05-16 09:23:17 +0430

    Add new colors

--- app/src/main/java/ir/erfansn/siliconecalculator/ui/theme/Color.kt
+++ app/src/main/java/ir/erfansn/siliconecalculator/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package ir.erfansn.siliconecalculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val BlueGrey50 = Color(0xFFECECEC)
+val BlueGrey100 = Color(0xFFCFD3DE)
+val BlueGrey300 = Color(0xFFA0A8BB)
+val BlueGrey600 = Color(0xFF5B5F6A)
+val BlueGrey700 = Color(0xFF474D5B)
+val BlueGrey800 = Color(0xFF333D49)
+val BlueGrey900 = Color(0xFF2A2E39)
+val DeepOrange800 = Color(0xFFC25A11)
+val DeepOrange900 = Color(0xFFC05F1C)
