f684914c66b4b18d58792ab0aae47b8e4b00e60a
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-07-22 12:38:50 +0430

    Merge HistoryLocalDataSource with HistoryRepositoryImpl

--- app/src/main/java/ir/erfansn/siliconecalculator/data/repository/HistoryRepositoryImpl.kt
+++ app/src/main/java/ir/erfansn/siliconecalculator/data/repository/HistoryRepositoryImpl.kt
@@ -2,27 +2,33 @@ package ir.erfansn.siliconecalculator.data.repository
 
 import ir.erfansn.siliconecalculator.data.model.Computation
 import ir.erfansn.siliconecalculator.data.model.asHistoryEntity
-import ir.erfansn.siliconecalculator.data.source.local.HistoryLocalDataSource
+import ir.erfansn.siliconecalculator.data.source.local.db.dao.HistoryDao
 import ir.erfansn.siliconecalculator.data.source.local.db.model.HistoryEntity
 import ir.erfansn.siliconecalculator.data.source.local.db.model.asHistoryItem
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class HistoryRepositoryImpl @Inject constructor(
-    private val historyLocalDataSource: HistoryLocalDataSource
+    private val historyDao: HistoryDao,
+    private val ioDispatcher: CoroutineDispatcher
 ) : HistoryRepository {
 
-    override val historyItemsStream = historyLocalDataSource
-        .historyEntitiesStream
+    override val historyItemsStream = historyDao.getHistoryEntitiesStream()
         .map {
             it.map(HistoryEntity::asHistoryItem)
         }
 
     override suspend fun clearAllHistory() {
-        historyLocalDataSource.clearHistoryEntities()
+        withContext(ioDispatcher) {
+            historyDao.deleteAllHistoryEntities()
+        }
     }
 
     override suspend fun saveComputation(computation: Computation) {
-        historyLocalDataSource.insertHistoryEntity(computation.asHistoryEntity())
+        withContext(ioDispatcher) {
+            historyDao.insertHistoryEntity(computation.asHistoryEntity())
+        }
     }
 }
