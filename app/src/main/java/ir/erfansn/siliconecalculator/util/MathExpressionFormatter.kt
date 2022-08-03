ef35efd6d2829782da35a5b5046d6718854b413a
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-08-03 19:29:59 +0430

    Modify MathExpressionFormatter

--- app/src/main/java/ir/erfansn/siliconecalculator/util/MathExpressionFormatter.kt
+++ app/src/main/java/ir/erfansn/siliconecalculator/util/MathExpressionFormatter.kt
@@ -1,12 +1,16 @@
 package ir.erfansn.siliconecalculator.util
 
-private val DECIMAL_NUMBER_REGEX = """(-)?(0|\d*)(\.)?(\d*)(E)?(-)?(\d+)?""".toRegex()
+const val OPERATORS_REGEX = """[+\-×÷]"""
+const val DECIMAL_REGEX = """-?\d+\.?(?:\d+(?:E-?\d+)?)?"""
+
+private const val SPECIFIC_NUMBER_REGEX = """(-)?(0|\d*)(\.)?(\d*)(E-?)?(\d+)?"""
 
 fun String.formatNumbers(): String {
     fun String.separateByComma() = reversed().chunked(3).joinToString(",").reversed()
 
-    return replace(DECIMAL_NUMBER_REGEX) {
-        val (integerSign, integer, point, fraction, e, exponentSign, exponent) = it.destructured
-        "$integerSign${integer.separateByComma()}$point$fraction$e$exponentSign${exponent.separateByComma()}"
+    return replace(SPECIFIC_NUMBER_REGEX.toRegex()) {
+        val (integerSign, integer, point, fraction, exponentSign, exponent) = it.destructured
+
+        "$integerSign${integer.separateByComma()}$point$fraction$exponentSign${exponent.separateByComma()}"
     }
 }
