3e6277a5d335aba320e6bfc1882a3377b7c88ce7
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-08-04 17:36:50 +0330

    Set system bar color inside SideEffect

--- app/src/main/java/ir/erfansn/siliconecalculator/ui/theme/Theme.kt
+++ app/src/main/java/ir/erfansn/siliconecalculator/ui/theme/Theme.kt
@@ -21,6 +21,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -54,11 +55,13 @@ fun SiliconeCalculatorTheme(
     content: @Composable () -> Unit
 ) {
     val systemUi = rememberSystemUiController()
-    systemUi.setSystemBarsColor(
-        color = Color.Transparent,
-        darkIcons = !darkTheme,
-        isNavigationBarContrastEnforced = false
-    )
+    SideEffect {
+        systemUi.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = !darkTheme,
+            isNavigationBarContrastEnforced = false
+        )
+    }
 
     val colors = if (darkTheme) {
         DarkColorPalette
