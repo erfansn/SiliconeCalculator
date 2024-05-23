2babc334c60f357cdfd8facc51a441c8cab15b15
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-05-23 16:17:49 +0330

    Update SDK version to 34

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -16,8 +16,8 @@
 
 object Configs {
     const val PACKAGE_NAME = "ir.erfansn.siliconecalculator"
-    const val COMPILE_SDK_VERSION = 33
+    const val COMPILE_SDK_VERSION = 34
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 33
+    const val TARGET_SDK_VERSION = 34
     const val JVM_TOOLCHAIN_VERSION = 17
 }
