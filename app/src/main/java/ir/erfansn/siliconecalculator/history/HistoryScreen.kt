6fdfad5e1d1ddc7e230725843c604368767a936a
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-07-04 17:33:56 +0330

    Fix problem of predictive back handler

--- app/src/main/java/ir/erfansn/siliconecalculator/history/HistoryScreen.kt
+++ app/src/main/java/ir/erfansn/siliconecalculator/history/HistoryScreen.kt
@@ -21,14 +21,38 @@ package ir.erfansn.siliconecalculator.history
 import android.content.res.Configuration
 import androidx.activity.compose.PredictiveBackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.ClearAll
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -68,14 +92,10 @@ fun HistoryScreen(
         initialValue = ModalBottomSheetValue.Hidden
     )
 
-    PredictiveBackHandler {
+    PredictiveBackHandler(enabled = clearHistoryBottomSheetState.isVisible) {
         try {
             it.collect()
-            if (clearHistoryBottomSheetState.isVisible) {
-                coroutineScope.launch { clearHistoryBottomSheetState.hide() }
-            } else {
-                onBackPress()
-            }
+            coroutineScope.launch { clearHistoryBottomSheetState.hide() }
         } catch (e: CancellationException) {
             // Nothing to do
         }
